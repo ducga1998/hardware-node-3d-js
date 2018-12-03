4763ee3e04f137afed76829b73f37a7a1edddfd7
Ducnm <duc2820@gmail.com>
2018-12-03 15:44:20 +0700

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var numberPort = process.env.PORT || 3001;
+// var asyncPort = require('./serialPort')
+var SerialPort = require('serialport');
+const fs = require('fs')
+const dev = '/dev/cu.usbmodem14101'
+let port
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+fs.watchFile(dev, () => {
+  const connected = fs.existsSync(dev)
+  if (connected) {
+    console.log('connected')
+    port = new SerialPort(dev, {
+      baudRate: 115200
+    })
+    port.on('data', (buf) => {
+      io.sockets.emit('testIot', buf.toString())
+    })
+  } else {
+    console.log('disconnected')
+  }
+});
+// const port = await asyncPort
+// if (port) {
+//   port.on('data', (data) => {
+
+//     // console.log(data.toString())
+//     io.sockets.emit('testIot', data.toString())
+//   })
+// }
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/view/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+http.listen(numberPort, function () {
+  console.log('listening on *:' + numberPort);
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+
+
+
+
+
+
